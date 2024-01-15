fd9ed41efb9f6395f0d59bba3f0a90d25c7f418d
LuthfiAsmara <luthfigooners29@gmail.com>
2024-01-16 00:52:09 +0700

    init project

---
+++ Soker/App/SokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SokerApp.swift
+//  Soker
+//
+//  Created by Luthfi Asmara on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
